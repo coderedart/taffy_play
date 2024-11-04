7a0456b3bf9ac2f85d76ab8420ed5171a94c2812
Red Artist <redartofcode@gmail.com>
2024-11-05 05:11:36 +0530

    fix wasm and README

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(taffy_play::TemplateApp::new(cc)))),
             )
             .await;
 
